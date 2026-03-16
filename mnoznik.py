45c6ce232cf6a8dcb00a4b1da47450837c48d953
Marcel <125443370+zdzimil@users.noreply.github.com>
2026-03-16 20:33:21 +0100

    Create mnoznik.py

---
+++ mnoznik.py
@@ -0,0 +1,14 @@
+"""Napisz funkcję wyższego rzędu, o nazwie stworz_mnoznik, która przyjmuje jeden argument x.
+Wewnątrz tej funkcji zdefiniuj drugą funkcję o nazwie mnoznik, która przyjmie argument y
+i zwróci iloczyn obu tych liczb. Funkcja główna powinna zwracać funkcję wewnętrzną - podpowiedź: precentacja"""
+
+"""Moje przykładowe 
+def stworz_mnoznik(liczba_x):
+    def mnoznik(liczba_y):
+        return liczba_x * liczba_y
+    return mnoznik
+
+pomnoz_przez_3 = stworz_mnoznik(3)
+
+print(pomnoz_przez_3(10))
+"""
