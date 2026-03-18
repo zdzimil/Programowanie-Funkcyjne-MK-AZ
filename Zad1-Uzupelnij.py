c5fc481b8462f89237af508cbc22c46687f3dac9
Marcel <125443370+zdzimil@users.noreply.github.com>
2026-03-18 13:18:36 +0100

    Update Zad1-Uzupelnij.py

--- Zad1-Uzupelnij.py
+++ Zad1-Uzupelnij.py
@@ -1,12 +1,12 @@
-"""1.Uzupełnij by poprawnie powiększyć dwukrotnie tane liczby"""
+"""Uzupełnij by poprawnie powiększyć dwukrotnie dane liczby"""
 liczby = [1, 2, 3, 4, 5]
 powiekszone_liczby = list(map(lambda x: ___, liczby))
 
 print(powiekszone_liczby)
 # Oczekiwany wynik to [2, 4, 6, 8, 10]
 
-"""2.Uzupełnij wyrażenie lambda oraz brakujący argument funkcji reduce, 
-aby obliczyć iloczyn (wynik mnożenia) wszystkich liczb w liście."""
+"""Uzupełnij wyrażenie lambda oraz brakujący argument funkcji reduce, 
+aby obliczyć iloczyn wszystkich liczb w liście."""
 from functools import reduce
 
 liczby = [1, 2, 3, 4, 5]
@@ -15,7 +15,7 @@ iloczyn = reduce(lambda x, y: ___, ___)
 print(iloczyn)
 # Oczekiwany wynik: 120
 
-"""3.Uzupełnij wywołanie funkcji sorted, przekazując odpowiednią listę oraz tworząc wyrażenie lambda,
+"""Uzupełnij wywołanie funkcji sorted, przekazując odpowiednią listę oraz tworząc lambdę,
  które posortuje słowa według ich długości"""
 
 slowa = ["Kapusta", "Kawa", "Fajerwerki", "Rurka"]
