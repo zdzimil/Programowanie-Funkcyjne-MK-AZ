2aaab49cf697d277960afe59a37d96df48c90a42
azupnik <126894640+azupnik@users.noreply.github.com>
2026-03-25 11:40:07 +0100

    Update Zad5-Filter_sorted.py

--- Zad5-Filter_sorted.py
+++ Zad5-Filter_sorted.py
@@ -2,6 +2,11 @@
 #2.	Sortowanie: Wykorzystaj funkcję sorted() na przefiltrowanej liście, aby uporządkować urządzenia według zużycia procesora (cpu_usage) w kolejności malejącej.
 #3.	Wyświetlanie: Wypisz wynikową listę w konsoli.
 # Format: {"host": nazwa, "status": stan, "cpu_usage": %, "uptime_dni": dni}
+#Poprawna odpowiedź:
+#{'host': 'Router_D', 'status': 'online', 'cpu_usage': 92, 'uptime_dni': 15}
+#{'host': 'Router_A', 'status': 'online', 'cpu_usage': 85, 'uptime_dni': 120}
+#{'host': 'Router_C', 'status': 'online', 'cpu_usage': 40, 'uptime_dni': 350}
+#{'host': 'Router_E', 'status': 'online', 'cpu_usage': 15, 'uptime_dni': 200}
 urzadzenia = [
     {"host": "Router_A", "status": "online", "cpu_usage": 85, "uptime_dni": 120},
     {"host": "Router_B", "status": "offline", "cpu_usage": 0, "uptime_dni": 0},
