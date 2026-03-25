f9a96f77c8ecfc8c179fde5e56934225dee90635
azupnik <126894640+azupnik@users.noreply.github.com>
2026-03-25 10:30:06 +0100

    Correct slide number in calculator instructions

--- Zad2-Kalkulator.py
+++ Zad2-Kalkulator.py
@@ -1,5 +1,5 @@
 """
-Na slajdzie 11 pokazano kalkulator. Twoim zadaniem jest jego rozbudowa.
+Na slajdzie 10 pokazano kalkulator. Twoim zadaniem jest jego rozbudowa.
 Stwórz pięć czystych funkcji: dla dodawania, odejmowania, mnożenia, dzielenia i potęgowania.
 Zbuduj słownik, który jako klucze przyjmie znaki "+", "-", "*", "/", "^", a jako wartości przypisze odpowiednie funkcje.
 """
