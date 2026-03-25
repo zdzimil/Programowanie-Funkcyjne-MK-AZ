954845c14892eed2691449b67f274fb93cfec80f
Marcel <125443370+zdzimil@users.noreply.github.com>
2026-03-25 12:45:00 +0100

    zadania

--- zadania/Zad3-mnoznik.py
+++ zadania/Zad3-mnoznik.py
@@ -1,3 +1,3 @@
 """Napisz funkcję wyższego rzędu, o nazwie stworz_mnoznik, która przyjmuje jeden argument x.
 Wewnątrz tej funkcji zdefiniuj drugą funkcję o nazwie mnoznik, która przyjmie argument y
-i zwróci iloczyn obu tych liczb. Funkcja główna powinna zwracać funkcję wewnętrzną - prezentacja - funkcje wyższego rzędu"""
+i zwróci iloczyn obu tych liczb. Funkcja główna powinna zwracać funkcję wewnętrzną- prezentacja - funkcje wyższego rzędu"""
